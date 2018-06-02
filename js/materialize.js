23736e5929056a5553c41898148069f78fe43761
Handrew <andrew.a.han@gmail.com>
2018-06-02 14:47:47 -0400

    basic outline of about me section with image

--- js/materialize.js
+++ js/materialize.js
@@ -1,14 +1,22 @@
+$(document).ready(function(){
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("nav").offset().top > 100) {
       $("nav").addClass("navShrink");
       $("#nav-mobile").addClass("shrink");
+      $(".one").addClass("oneShrink");
     } else {
       $("nav").removeClass("navShrink");
       $("#nav-mobile").removeClass("shrink");
+      $(".one").removeClass("oneShrink");
     }
+
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+  
+    $('.carousel').carousel();
+  });
