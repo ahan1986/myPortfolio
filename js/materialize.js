5382f2e4943ff1ceadfe17f63695aa9ba2b6d8c8
Handrew <andrew.a.han@gmail.com>
2018-06-02 10:46:58 -0400

    create shrink for navbar

--- js/materialize.js
+++ js/materialize.js
@@ -1,3 +1,14 @@
-// $(document).ready(function(){
-//     $('.parallax').parallax();
-//   });
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("nav").offset().top > 100) {
+      $("nav").addClass("navShrink");
+      $("#nav-mobile").addClass("shrink");
+    } else {
+      $("nav").removeClass("navShrink");
+      $("#nav-mobile").removeClass("shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
